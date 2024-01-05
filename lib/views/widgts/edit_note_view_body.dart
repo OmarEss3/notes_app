be0bd132166c332c309676698d71818a277637ca
OmarEss3 <omaresam2323@duck.com>
2024-01-05 13:55:56 +0200

    edit note view ui done

--- lib/views/widgts/edit_note_view_body.dart
+++ lib/views/widgts/edit_note_view_body.dart
@@ -1,11 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/views/widgts/CustomAppBar.dart';
+import 'package:note_app/views/widgts/custom_text_field.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(padding: EdgeInsets.symmetric(horizontal: 24,),child: Column(children: [SizedBox(height: 50,),CustomAppBar(title: 'Edit Note', icon: Icons.check)],),);
+    return const Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: 24,
+      ),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 50,
+          ),
+          CustomAppBar(title: 'Edit Note', icon: Icons.check),
+          SizedBox(
+            height: 50,
+          ),
+          CustomTextField(hint: 'title'),
+          SizedBox(
+            height: 20,
+          ),
+          CustomTextField(
+            hint: 'Content',
+            maxLines: 5,
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/views/widgts/notes_view_body.dart
+++ lib/views/widgts/notes_view_body.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:note_app/views/widgts/CustomAppBar.dart';
-import 'package:note_app/views/widgts/custom_note_item.dart';
 import 'package:note_app/views/widgts/notes_list_view.dart';
 
 class NotesViewBody extends StatelessWidget {
