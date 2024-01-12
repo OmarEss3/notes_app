0f9609af79142a945f4995206013bad59bcacf46
OmarEss3 <omaresam2323@duck.com>
2024-01-12 15:44:14 +0200

    create notes state

--- lib/views/widgts/add_note_bottem_sheet.dart
+++ lib/views/widgts/add_note_bottem_sheet.dart
@@ -23,15 +23,13 @@ class AddNoteBottomSheet extends StatelessWidget {
         builder: (context, state) {
           return AbsorbPointer(
             absorbing: state is AddNotesLoading ? true : false,
-            child: ModalProgressHUD(
-              inAsyncCall: state is AddNotesLoading ? true : false,
-              child: const Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: 16,
-                ),
-                child: SingleChildScrollView(
-                  child: AddNoteFormState(),
-                ),
+            child: Padding(
+              padding: EdgeInsets.only(
+                  left: 16,
+                  right: 16,
+                  bottom: MediaQuery.of(context).viewInsets.bottom),
+              child: const SingleChildScrollView(
+                child: AddNoteFormState(),
               ),
             ),
           );
